1409ff3ddc93966e5de38d1991dccf90be166293
Kaik Aciole <kaikaciole1.0@gmail.com>
2026-08-12 21:46:30 -0300

    feat: cria entidades de veiculo e concessionaria

---
+++ backend/dealership-management/src/main/java/br/com/accenture/dealership_management/infrastructure/adapter/out/persistence/entity/DealershipEntity.java
@@ -0,0 +1,51 @@
+package br.com.accenture.dealership_management.infrastructure.adapter.out.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "dealership")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DealershipEntity {
+
+    @Id
+    private UUID id;
+
+    @Column(nullable = false)
+    private String corporateName;
+
+    @Column(nullable = false, length = 14, unique = true)
+    private String cnpj;
+
+    @Column(nullable = false, length = 8)
+    private String cep;
+
+    @Column(nullable = false)
+    private String street;
+
+    @Column(nullable = false)
+    private String neighborhood;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false, length = 2)
+    private String state;
+
+    private LocalDate foundationDate;
+
+    @Column(nullable = false)
+    private boolean isActive;
+}
