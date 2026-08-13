5a3670526846e1ccb7680d6e95c876758f770289
Kaik Aciole <kaikaciole1.0@gmail.com>
2026-08-12 21:48:44 -0300

    feat: cria interface repository de veiculo e concessionaria

---
+++ backend/dealership-management/src/main/java/br/com/accenture/dealership_management/infrastructure/adapter/out/persistence/repository/SpringDataVehicleRepository.java
@@ -0,0 +1,9 @@
+package br.com.accenture.dealership_management.infrastructure.adapter.out.persistence.repository;
+
+import br.com.accenture.dealership_management.infrastructure.adapter.out.persistence.entity.VehicleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface SpringDataVehicleRepository extends JpaRepository<VehicleEntity, UUID> {
+}
