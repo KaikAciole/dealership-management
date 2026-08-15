69573d515ba44bf72a486624cdda7027fcc9d5bf
Kaik Aciole <kaikaciole1.0@gmail.com>
2026-08-15 09:26:51 -0300

    fix: desacopla modelo de dominio dos dtos de requisicao de concessionaria

---
+++ backend/dealership-management/src/main/java/br/com/accenture/dealership_management/infrastructure/adapter/in/web/dto/request/AddressRequest.java
@@ -0,0 +1,10 @@
+package br.com.accenture.dealership_management.infrastructure.adapter.in.web.dto.request;
+
+public record AddressRequest(
+        String cep,
+        String street,
+        String city,
+        String state,
+        String neighborhood
+) {}
+
